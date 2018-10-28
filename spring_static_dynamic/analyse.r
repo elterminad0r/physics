2dd2daeb41bb405fe58a7f0ff23c57722b3f1f6d
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-10-28 22:21:59 +0000

    Finish spring constant

--- spring_static_dynamic/analyse.r
+++ spring_static_dynamic/analyse.r
@@ -1,66 +1,63 @@
 library(ggplot2)
 
-k = 1.3806488e-23
+U_x = 0.002
+pc_U_m = 5
+U_20T = 0.2
+U_T = U_20T / 20
 
-U_I = 0.01e-3
-U_T = 0.2
+g = 9.80665
 
-V_values <- c(5.11, 5.08, 5.15, 5.27)
-V <- mean(V_values)
-V
+static_df <- read.table("static.csv", sep=",", header=TRUE)
 
-current_df <- read.table("data.csv", sep=",")
-colnames(current_df) <- c("T_celsius", "I_ma")
+static_df$x <- rowMeans(subset(static_df, select = c(x1, x2, x3, x4, x5)))
+static_df$F <- g * static_df$m
 
-current_df$T <- current_df$T_celsius + 273.15
-current_df$I <- current_df$I_ma / 1000
+static_df
 
-current_df$T_recip = 1 / current_df$T
-current_df$ln_I = log(current_df$I)
+static_model <- lm(F ~ x, data=static_df)
+summary(static_model)
 
-pc_U_I = U_I / median(current_df$I)
-pc_U_T = U_T / median(current_df$T)
-
-pc_U_I
-pc_U_T
-
-current_df
-
-nl_model <- nls(I~A*exp(-E/(k * T)),
-                start=list(A=4, E=0.3 * 1.6e-19), data=current_df)
-nl_model
-
-qplot(T, I, data=current_df,
-      main="Current through thermistor against temperature",
-      xlab="T / K", ylab="I / A") +
-      geom_errorbar(data=current_df, mapping=aes(ymin=I-U_I, ymax=I+U_I)) +
-      geom_errorbarh(data=current_df, mapping=aes(xmin=T-U_T, xmax=T+U_T)) +
+qplot(x, F, data=static_df,
+      main="Force against extension in a spring (Hooke's Law)",
+      xlab="x / m", ylab="F / N") +
+      geom_errorbar(mapping=aes(ymin=g * (m * (100 + pc_U_m) / 100),
+                                ymax=g * (m * (100 - pc_U_m) / 100),
+                                width=0.005)) +
+      geom_errorbarh(mapping=aes(xmin=x - U_x,
+                                 xmax=x + U_x,
+                                 height=0.1)) +
       theme(panel.grid.minor = element_line(colour="gray", size=0.4),
             panel.grid.major = element_line(colour="gray", size=1),
             panel.background = element_blank()) +
-      scale_y_continuous(minor_breaks = seq(0.1e-3, 1.1e-3, 0.2e-4),
-                         breaks = seq(0.1e-3, 1.1e-3, 0.1e-3)) +
-      scale_x_continuous(minor_breaks = seq(280, 320, 1),
-                         breaks = seq(280, 320, 5)) +
-      geom_line(aes(y = predict(nl_model)), color="steelblue")
+      scale_y_continuous(minor_breaks = seq(0, 7, 0.2),
+                         breaks = seq(0, 7, 1)) +
+      scale_x_continuous(minor_breaks = seq(0.5, 1, 0.01),
+                         breaks = seq(0.5, 1, 0.05)) +
+      geom_smooth(method = "lm", color = "red", size=.1)
 
-model <- lm(ln_I ~ T_recip, data=current_df)
-model
+dynamic_df <- read.table("dynamic.csv", sep=",", header=TRUE)
+dynamic_df$T <- rowMeans(subset(dynamic_df, select=c(T1, T2, T3, T4, T5))) / 20
+dynamic_df$plot_y <- (2 * pi / dynamic_df$T) ^ 2
+dynamic_df$plot_x <- 1 / dynamic_df$m
 
-qplot(T_recip, ln_I, data=current_df,
-      main="ln(I) against 1/T",
-      xlab="K / T", ylab="ln(I) - ln(A)") +
-      geom_errorbar(data=current_df,
-                    mapping=aes(ymin=ln_I - pc_U_I,
-                                ymax=ln_I + pc_U_I)) +
-      geom_errorbarh(data=current_df,
-                     mapping=aes(xmin=(1 - pc_U_T) * T_recip,
-                                 xmax=(1 + pc_U_T) * T_recip)) +
+dynamic_df
+
+dynamic_model <- lm(plot_y ~ plot_x, data=dynamic_df)
+summary(dynamic_model)
+
+qplot(plot_x, plot_y, data=dynamic_df,
+      main="Dynamic analysis of spring constant: Period against mass",
+      xlab="kg / m", ylab="(Angular Frequency)^2/Hz^2") +
+      geom_errorbar(mapping=aes(ymin=(2 * pi / (T + U_T))^2,
+                                ymax=(2 * pi / (T - U_T))^2),
+                                width=0.2) +
+      geom_errorbarh(mapping=aes(xmin=1 / (m * (100 + pc_U_m) / 100),
+                                 xmax=1 / (m * (100 - pc_U_m) / 100))) +
       theme(panel.grid.minor = element_line(colour="gray", size=0.4),
             panel.grid.major = element_line(colour="gray", size=1),
             panel.background = element_blank()) +
-      scale_y_continuous(minor_breaks = seq(-10, -5, 0.05),
-                         breaks = seq(-10, -5, 0.2)) +
-      scale_x_continuous(minor_breaks = seq(0.003, 0.004, 0.00002),
-                         breaks = seq(0.003, 0.004, 0.0001)) +
-      geom_line(aes(y = predict(model)), color="steelblue")
+      scale_y_continuous(minor_breaks = seq(0, 250, 10),
+                         breaks = seq(0, 250, 50)) +
+      scale_x_continuous(minor_breaks = seq(0, 11, 0.4),
+                         breaks = seq(0, 11, 2)) +
+      geom_smooth(method = "lm", color = "steelblue", size=.1)
